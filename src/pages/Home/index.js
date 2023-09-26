d7116bdab54963fb66d730febea454619733d69c
Jola Ajayi <benjamen.ajayi@gmail.com>
2023-09-25 22:51:19 -0400

    Updated my email

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,7 @@ import MobileNavbar from "../../components/MobileNavbar";
 export default function Home(){
     const githubUrl = "https://github.com/jola442";
     const linkedInUrl = "https://www.linkedin.com/in/jola-ajayi/"
-    const email = "mailto:benjamen.ajayi@gmail.com"
+    const email = "mailto:jolaajayi@cmail.carleton.ca"
     const resume = "./Jola Ajayi's Resume.pdf"
     return (
         <>
